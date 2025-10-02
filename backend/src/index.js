66c4e3e725ba9278c2f9c04170067c7a634ba013
Aayush Kumar <ayushbhardwaj9504@gmail.com>
2025-10-03 00:02:06 +0530

    fixed the limit using in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 job.start();
 app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 // routes
